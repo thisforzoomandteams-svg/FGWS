4807d261c3782dea331b6123dd1036648586ecef
thisforzoomandteams-svg <thisforzoomandteams@gmail.com>
2026-03-12 20:16:43 +0400

    Remove Imported folder - files already in src/

--- FGWS/build/Imported/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: C:/Users/yomna/OneDrive/Desktop/Projects/FGWS/Imported
-# Build directory: C:/Users/yomna/OneDrive/Desktop/Projects/FGWS/build/Imported
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
